c96797ab5256caf63d8b4805abf2195daeaed1bb
Gensun <1412912425@qq.com>
2017-04-01 16:16:38 +0800

    first commit

--- SkStore/ViewController.m
+++ SkStore/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <StoreKit/StoreKit.h>
 
-@interface ViewController ()
+@interface ViewController ()<SKStoreProductViewControllerDelegate>
 
 @end
 
@@ -17,8 +18,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //[self firstWay];
+    [self secondWay];
 }
 
+- (void)firstWay
+{
+    NSMutableDictionary *parameter = [[NSMutableDictionary alloc] init];
+    [parameter setValue:[NSNumber numberWithInt:742562928] forKey:SKStoreProductParameterITunesItemIdentifier];
+        SKStoreProductViewController *storeViewController = [[SKStoreProductViewController alloc] init];
+        storeViewController.delegate = self;
+        [storeViewController loadProductWithParameters:parameter completionBlock:^(BOOL result, NSError * _Nullable error) {
+            if (result) {
+                [self presentViewController:storeViewController animated:YES completion:nil];
+            } else {
+                NSLog(@"NO SUCCESS LOADING PRODUCT SCREEN");
+            }
+        }];
+}
+
+- (void) secondWay
+{
+    if ([UIDevice currentDevice].systemVersion.floatValue >= 10.3) {
+        [SKStoreReviewController requestReview];
+    }
+}
+
+- (void)productViewControllerDidFinish:(SKStoreProductViewController *)viewController
+{
+    [viewController dismissViewControllerAnimated:YES completion:^{
+        
+        
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
